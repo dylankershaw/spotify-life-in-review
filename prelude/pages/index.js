c1322534cd39ddbbc63753e159930ca0e47fc944
dylankershaw <dylankershaw@me.com>
2020-03-09 10:39:37 -0400

    initial next.js app commit

---
+++ prelude/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Life in Review</title>
+      <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
+      <link rel='icon' href='/favicon.ico' type='image/x-icon' />
+    </Head>
+
+    <main></main>
+
+    <footer></footer>
+  </div>
+);
+
+export default Home;
