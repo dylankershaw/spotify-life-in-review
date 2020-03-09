883bfe0905ddb1c1700103e3654c27d534e2ac4e
dylankershaw <dylankershaw@me.com>
2020-03-09 17:48:29 -0400

    add played in full checkbox

--- prelude/components/Chart.jsx
+++ prelude/components/Chart.jsx
@@ -3,22 +3,23 @@ import Chart from 'chart.js';
 
 export default ({songData, selectedArtist}) => {
   const [chartData, setChartData] = useState([]);
+  const [fullPlaysOnly, setFullPlaysOnly] = useState(false);
   const ref = useRef(null);
 
   useEffect(() => {
     if (!selectedArtist) return setChartData([]);
 
-    const songsWithArtist = songData.filter(s =>
-      s.master_metadata_album_artist_name?.includes(selectedArtist)
-    );
+    let songs = songData.filter(s => s.master_metadata_album_artist_name?.includes(selectedArtist));
 
-    const data = songsWithArtist.reduce((acc, cur) => {
+    if (fullPlaysOnly) songs = songs.filter(({reason_end}) => reason_end === 'trackdone');
+
+    const data = songs.reduce((acc, cur) => {
       const date = cur.ts.split(' ')[0];
       return {...acc, [date]: (acc[date] || 0) + 1};
     }, {});
 
     setChartData(data);
-  }, [songData, selectedArtist]);
+  }, [songData, selectedArtist, fullPlaysOnly]);
 
   useEffect(() => {
     const myChartRef = ref.current.getContext('2d');
@@ -42,6 +43,13 @@ export default ({songData, selectedArtist}) => {
 
   return (
     <div>
+      <input
+        id='full-plays-only'
+        type='checkbox'
+        checked={fullPlaysOnly}
+        onChange={e => setFullPlaysOnly(e.target.checked)}
+      />
+      <label htmlFor='full-plays-only'>Only include songs played in full</label>
       <canvas id='myChart' ref={ref} />
     </div>
   );
