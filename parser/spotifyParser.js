8655700cbe7fc94f3227c9c82fb34c0950bad0ed
dylankershaw <dylankershaw@me.com>
2020-03-18 13:17:21 -0400

    prettier

--- parser/spotifyParser.js
+++ parser/spotifyParser.js
@@ -1,24 +1,28 @@
 const fs = require('fs');
-const histogram = require('ascii-histogram');
+// const histogram = require('ascii-histogram');
 
-const buffer = fs.readFileSync('EndSong.json');
+const buffer = fs.readFileSync("EndSong_comma_delimited.json");
 const songList = JSON.parse(buffer);
 
-const aAndBSongs = songList.filter(s => s.master_metadata_album_artist_name && s.master_metadata_album_artist_name.includes("Above \u0026 Beyond"));
+const aAndBSongs = songList.filter(
+  ({ master_metadata_album_artist_name: artist = "" }) =>
+    artist.includes("Above \u0026 Beyond")
+);
 
-// console.table(aAndBSongs, ["ts", "master_metadata_track_name", "master_metadata_album_album_name"])
+const firstCompletedSong = aAndBSongs
+  .reverse()
+  .find(({ reason_end }) => reason_end === "trackdone");
 
-// const firstCompletedSong = aAndBSongs.reverse().find(s => s.reason_end === "trackdone")
-// console.log(firstCompletedSong)
+console.log(firstCompletedSong);
 
 // const firstListenDate = aAndBSongs[aAndBSongs.length - 1].ts.split(" ")[0];
 // const lastListenDate = aAndBSongs[0].ts.split(" ")[0];
 
 // console.log(firstListenDate, lastListenDate)
 
-const histogramData = aAndBSongs.reduce((acc, cur) => {
-	const date = cur.ts.split(" ")[0];
-	return { ...acc, [date]: (acc[date] || 0) + 1 };
-}, {});
+// const histogramData = aAndBSongs.reduce((acc, cur) => {
+//   const date = cur.ts.split(" ")[0];
+//   return { ...acc, [date]: (acc[date] || 0) + 1 };
+// }, {});
 
 // console.log(histogram(histogramData));
